e1a266f0a26a9800ed3ce9cc6f3675e51a5dd34e
jifrozen0110 <answldjs1836@gmail.com>
2024-12-07 04:15:49 +0900

    loadBalancer 설정

---
+++ gateway/src/test/kotlin/com/puzzle/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.puzzle.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
